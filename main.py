07fd910d2cf61162c6f1bcf5e9bd1df8b689570f
Stanislav Voitkus <stanislav.voitkus@outlook.com>
2023-05-21 00:22:23 +0300

    added: db, pics, song, jquery, js script to change src of pic and audio, some html changes

--- main.py
+++ main.py
@@ -8,9 +8,15 @@ app = Flask(__name__, template_folder="template")
 @app.route('/', methods=['POST', 'GET'])
 def main():
     if request.method == 'POST':
-        song = request.form['first-name']
+        song = request.form['song']
+        print(song)
+        song = song.lower()
         data = db.get_data(song)
-        return render_template("info.html")
+        path_song = "../songs/" + data[0][0] + ".mp3"
+
+        path_cover = "/static/pics/" + data[0][1].replace(" ", "") + ".png"
+
+        return render_template("info.html", data=data, path_song=path_song, path_cover=path_cover)
     else:
         return render_template("main.html")
 
